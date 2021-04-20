e88c42c801582f058e023ed5f281c29a4199c205
Nuru Mohamed <nurucs@users.noreply.github.com>
2021-04-20 14:41:57 +0300

    (refactor) folders structured

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,4 +1,4 @@
-import Home from './home';
+import Home from '../unAuthHome/home';
 
 function App() {
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/root/App';
 
 ReactDOM.render(
   <>
