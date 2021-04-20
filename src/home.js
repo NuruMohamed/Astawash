61c042584a32b619587282638c31814d919b883e
Nuru Mohamed <nurucs@users.noreply.github.com>
2021-04-20 09:33:03 +0300

    (style, refactor)landing page styled and two div containers added in the html

--- src/home.js
+++ src/home.js
@@ -1,18 +1,24 @@
 import noteImage from './note.png'
 import facebook from './facebook.svg'
+import './home.css';
+
 const Home = () => {
 
     return (
         <div id="unauth-home-container">
-            <div id="title">Astawash</div>
-            <img src={noteImage} />
-            <div id="description">Note taking has become super easy</div>
-            <div id="login-button">
-                <div id="login-title">Login</div> 
-                <img src={facebook}/>
+            <div id="container-1">
+                <div id="title">Astawash</div>
+                <img src={noteImage} />
+                <div id="description">Your Notes partner</div>
             </div>
-            <div id="signup">
-                or <label> signup </label>
+            <div id="container-2">
+                <div id="login-button">
+                    <div id="login-title">Login</div>
+                    <img src={facebook} />
+                </div>
+                <div id="signup">
+                    or <label> signup </label>
+                </div>
             </div>
         </div>
     );
